b2a7ec7da37041b5fce7e8a2020365ad25b07adc
joaosantosmedeiros <jopesame@gmail.com>
2024-06-04 10:48:04 -0300

    initial commit

---
+++ src/main/java/com/jota/literalura/LiterAluraChallengeApplication.java
@@ -0,0 +1,18 @@
+package com.jota.literalura;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiterAluraChallengeApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiterAluraChallengeApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
